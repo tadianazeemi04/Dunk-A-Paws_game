73c9f54ec89d218b9d4dab1311b656fdadb28271
Tadian Ahmad Azeemi <tadianazeemi04@gmail.com>
2026-09-14 16:35:11 +0500

    Initial Commit

---
+++ Game/GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameApp.swift
+//  Game
+//
+//  Created by Tadian Ahmad Azeemi on 14/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
